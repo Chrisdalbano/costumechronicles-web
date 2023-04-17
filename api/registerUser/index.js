acacc2666a4e4ed746eb3622be66db4b051d0a73
Desmin <chrisdalbano12@gmail.com>
2023-04-17 18:40:50 -0400

    modified index.js for error handling again

--- api/registerUser/index.js
+++ api/registerUser/index.js
@@ -1,57 +1,64 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const mysql = require("mysql2/promise");
 const bcrypt = require("bcrypt");
 require("dotenv").config();
 
 module.exports = async function (context, req) {
-  const { username, email, password } = req.body;
+  console.log("Request body:", req.body);
+  console.log("Environment variables:", process.env);
 
-  if (!password || password.trim() === '') {
-    context.res = {
-      status: 400,
-      body: "Password cannot be empty.",
-    };
-    return;
-  }
+  module.exports = async function (context, req) {
+    const { username, email, password } = req.body;
 
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
+    if (!password || password.trim() === "") {
+      context.res = {
+        status: 400,
+        body: "Password cannot be empty.",
+      };
+      return;
+    }
 
-    console.log("Connecting to the database...");
-    const connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      ssl: {
-        ca: fs.readFileSync(path.join(__dirname, 'DigiCertGlobalRootCA.crt.pem')),
-      },
-    });
-    console.log("Connected to the database");
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-    const [rows] = await connection.execute(
-      "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
-      [username, email, hashedPassword]
-    );
+      console.log("Connecting to the database...");
+      const connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        ssl: {
+          ca: fs.readFileSync(
+            path.join(__dirname, "DigiCertGlobalRootCA.crt.pem")
+          ),
+        },
+      });
+      console.log("Connected to the database");
 
-    await connection.end();
+      const [rows] = await connection.execute(
+        "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+        [username, email, hashedPassword]
+      );
 
-    context.res = {
-      status: 200,
-      body: "User registered successfully!",
-    };
-  } catch (error) {
-    console.error("Error:", error);
-    context.res = {
-      status: 500,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        error: true,
-        message: "Error: " + error.message,
-      }),
-    };
-  }
+      await connection.end();
+
+      context.res = {
+        status: 200,
+        body: "User registered successfully!",
+      };
+    } catch (error) {
+      console.error("Error:", error);
+      context.res = {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          error: true,
+          message: "Error: " + error.message,
+        }),
+      };
+    }
+  };
 };

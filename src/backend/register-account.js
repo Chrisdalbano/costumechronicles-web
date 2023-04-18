9aa402007674bf7cb1d376567acada45fc0eae2c
Desmin <chrisdalbano12@gmail.com>
2023-04-18 13:16:11 -0400

    modified register-account.js for error catching

--- src/backend/register-account.js
+++ src/backend/register-account.js
@@ -1,10 +1,11 @@
 document.getElementById("register-form").addEventListener("submit", async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.getElementById("username").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
+  try {
     const response = await fetch("/api/register", {
       method: "POST",
       headers: {
@@ -13,11 +14,23 @@ document.getElementById("register-form").addEventListener("submit", async (event
       body: JSON.stringify({ username, email, password }),
     });
 
-    const result = await response.json();
+    // Log the response status and headers for debugging
+    console.log("Response status:", response.status);
+    console.log("Response headers:", response.headers);
 
-    if (response.status === 200) {
-      alert("User registered successfully!");
-    } else {
-      alert("Error: " + result.message);
+    // Check if the response status is not ok (e.g., not a 2xx status)
+    if (!response.ok) {
+      // Log the response text for debugging
+      const responseText = await response.text();
+      console.error("Response text:", responseText);
+      throw new Error("Failed to register the user.");
     }
-  });
+
+    const result = await response.json();
+    console.log("Response data:", result);
+    alert("User registered successfully!");
+  } catch (error) {
+    console.error("Error:", error);
+    alert("There was an error registering the user.");
+  }
+});
